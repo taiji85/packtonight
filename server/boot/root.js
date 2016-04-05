04a088a7b9b74e9c211839587e1664e8ce742ef8
taiji85 <onlytaiji85@empal.com>
2016-04-05 18:40:42 -0400

    Basic Model setup

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,6 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
-  var router = server.loopback.Router();
+  /*var router = server.loopback.Router();
   router.get('/', server.loopback.status());
-  server.use(router);
+  server.use(router);*/
 };
