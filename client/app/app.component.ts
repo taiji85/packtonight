34e0894cca553598c62317e8dad33fd400d7c0e6
taiji85 <onlytaiji85@empal.com>
2016-04-05 23:37:36 -0400

    Angular 2 QuickStart Integration

---
+++ client/app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by joon on 16. 4. 5.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
---
+++ client/app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by joon on 16. 4. 5.
+ */
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
